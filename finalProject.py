b2ca30c3ddc7b05a25032d61495e8cf3f944ea9e
nacho <nacho.itc@gmail.com>
2021-01-07 12:02:32 +0100

    second round of agile dev: added templates and forms, added dummy data as placeholder

--- finalProject.py
+++ finalProject.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, request, url_for, redirect, flash, jsonify
 app = Flask(__name__)
 
+
+### Fake data as placeholder until DB implementation ###
+#Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+### End fake data ###
+
 ### ROUTING ###
 """
 Method: showRestaurants()
@@ -12,7 +22,7 @@ route:
 @app.route('/')
 @app.route('/restaurants/')
 def showRestaurants():
-    return 'This page will show all the restaurants'
+    return render_template('restaurants.html', restaurants=restaurants)
 
 """
 Method: newRestaurant()
@@ -22,7 +32,7 @@ Route:
 """
 @app.route('/restaurant/new/')
 def newRestaurant():
-    return 'This page will be for making a new restaurant'
+    return render_template('newRestaurant.html')
 
 """
 Method: editRestaurant()
@@ -34,7 +44,7 @@ Params:
 """
 @app.route('/restaurant/<int:restaurant_id>/edit/')
 def editRestaurant(restaurant_id):
-    return 'This page will be for editing restaurant %s' % restaurant_id
+    return render_template('editRestaurant.html', restaurant_id=restaurant_id)
 
 """
 Method: deleteRestaurant()
@@ -46,7 +56,7 @@ Params:
 """
 @app.route('/restaurant/<int:restaurant_id>/delete/')
 def deleteRestaurant(restaurant_id):
-    return 'This page will be for deleting restaurant %s' % restaurant_id
+    return render_template('deleteRestaurant.html', restaurant=restaurant)
 
 """
 Method: showMenu()
@@ -60,7 +70,7 @@ Params:
 @app.route('/restaurant/<int:restaurant_id>/')
 @app.route('/restaurant/<int:restaurant_id>/menu/')
 def showMenu(restaurant_id):
-    return 'This page is the menu for restaurant %s' % restaurant_id
+    return render_template('menu.html', restaurant=restaurant, items=items)
 
 """
 Method: newMenuItem
@@ -72,7 +82,7 @@ Params:
 """
 @app.route('/restaurant/<int:restaurant_id>/menu/new/')
 def newMenuItem(restaurant_id):
-    return 'This page is for making a new Menu Item for restaurant %s' % restaurant_id
+    return render_template('newMenuItem.html', restaurant_id=restaurant_id)
 
 """
 Method: editMenuItem()
@@ -85,7 +95,7 @@ Params:
 """
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
-    return 'This page is for editing menu item %s' % menu_id
+    return render_template('editMenuItem.html', restaurant_id=restaurant_id, item=item)
 
 """
 Method: deleteMenuItem()
@@ -98,7 +108,9 @@ Params:
 """
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
-    return 'This page is for deleting menu item %s' % menu_id
+    return render_template('deleteMenuItem.html', restaurant_id=restaurant_id, item=item)
+
+### END ROUTING ###
 
 if __name__ == '__main__':
     # app.secret_key = 'super_secret_key'
